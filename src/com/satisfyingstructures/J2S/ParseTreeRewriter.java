b16f96c0da614cf27fc5ee7036fda25784b7af11
Torsten Louland <torsten.louland@satisfyingstructures.com>
2016-11-15 22:25:51 +0100

    Track changed intervals in ParseTreeWriter

--- src/com/satisfyingstructures/J2S/ParseTreeRewriter.java
+++ src/com/satisfyingstructures/J2S/ParseTreeRewriter.java
@@ -39,12 +39,212 @@ import java.util.Map;
 
 public class ParseTreeRewriter extends TokenStreamRewriter {
 
+    /// ChangedIntervalsList records an ordered normalised list of intervals in the rewriter's token stream that will be
+    /// rewritten. The list can be added to, tested, reset and regenerated.
+	class ChangedIntervalsList
+	{
+		final ArrayList<Interval> intervals = new ArrayList<>(256); // Normalised (overlapping/abutting entries merged), sorted
+
+		void rebuildFromRewriteOps(List<TokenStreamRewriter.RewriteOperation> opList)
+		{
+            intervals.clear();
+            /*  Don't have sufficient access to RewriteOperation subclasses to do this:
+            TokenStreamRewriter.RewriteOperation op;
+            TokenStreamRewriter.ReplaceOp rop;
+            TokenStreamRewriter.InsertOp iop;
+            int i, len, start, stop;
+            for (i = 0, len = opList.size(); i < len; i++)
+            {
+                if (null == (op = opList.get(i)))
+                    continue;
+                start = stop = op.index;
+                if (op instanceof ReplaceOp)
+                {
+                    rop = (ReplaceOp)op;
+                    stop = rop.lastIndex;
+                }
+                changed(start, stop);
+            }
+            */
+		}
+
+		void changed(int start, int stop)
+		{
+            // Rewriting in response to a parse tree walk is generally done linearly going forward, with occasional
+            // larger rewrites reaching further back as the tree walk steps out to wider scopes. Therefore, rather than
+            // use a binary search, we search linearly backwards from the end, implying from most recent.
+
+            // We scan backwards for to identify all existing changed intervals that we intersect plus those that abutt
+            // each end if present, then replace them with a combined interval. We also check if the changed interval is
+            // completely contained in a gap, and if so insert it. Note that Interval represents a closed interval -
+            // both ends included.
+
+            if (0 == intervals.size())
+            {
+                intervals.add(Interval.of(start, stop));
+                return;
+            }
+
+            // Make a test interval that is one larger on each end, so that we can test for intersection with intervals
+            // that abutt in before and after
+            Interval intervalChanged = Interval.of(start, stop);
+            Interval intervalToCatch = Interval.of(start > 0 ? start-1 : 0, stop + 1);
+            Interval interval;
+            int gapStart, gapStop;
+            int combinedStart = start;
+            int combinedStop = stop;
+            final int NO_INDEX = Integer.MAX_VALUE;
+            int intersectingIndexLast = NO_INDEX;
+            int intersectingIndexFirst = NO_INDEX;
+            int insertionIndex = NO_INDEX;
+            int i = intervals.size();
+
+            // Scan backwards for the first intersection with an interval or complete containment by a gap
+            gapStop = Integer.MAX_VALUE;
+            while (0 < i--)
+            {
+                interval = intervals.get(i);
+                // Look for an intersection
+                if (!intervalToCatch.disjoint(interval))
+                {
+                    if (interval.properlyContains(intervalChanged))
+                        return; // already included in changed intervals, so nothing more to do
+                    intersectingIndexFirst = intersectingIndexLast = i;
+                    if (combinedStop < interval.b)
+                        combinedStop = interval.b;
+                    if (combinedStart > interval.a)
+                        combinedStart = interval.a;
+                    break;
+                }
+                // Create trailing gap and test
+                gapStart = interval.b + 1;
+                if (start >= gapStart && stop <= gapStop)
+                {
+                    insertionIndex = i + 1; // insert before following entry
+                    break;
+                }
+                gapStop = interval.a - 1;
+            }
+            if (0 > i && insertionIndex == NO_INDEX)
+            {
+                gapStart = 0;
+                if (start >= gapStart && stop <= gapStop)
+                    insertionIndex = i + 1; // insert before following entry
+            }
+
+            // Continue scanning backwards until last of intersecting intervals
+            if (insertionIndex == NO_INDEX)
+            while (0 < i--)
+            {
+                interval = intervals.get(i);
+                // Accumulate if we have another intersection
+                if (!intervalToCatch.disjoint(interval))
+                {
+                    intersectingIndexFirst = i;
+                    if (combinedStart > interval.a)
+                        combinedStart = interval.a;
+                }
+                else
+                    break; // done
+            }
+
+            for (i = intersectingIndexLast; i > intersectingIndexFirst; i--)
+                intervals.remove(i);
+            if (intersectingIndexFirst != NO_INDEX && intersectingIndexFirst < intervals.size())
+                intervals.set(intersectingIndexFirst, Interval.of(combinedStart, combinedStop));
+            else
+            if (insertionIndex != NO_INDEX)
+                intervals.add(insertionIndex, Interval.of(combinedStart, combinedStop));
+		}
+
+		void changed(Interval interval)
+		{
+            changed(interval.a, interval.b);
+        }
+
+        boolean isChanged(Interval interval)
+        {
+            return isChanged(interval.a, interval.b);
+        }
+
+        boolean isChanged(int from, int to)
+        {
+            Interval interval;
+            int i = intervals.size();
+            while (0 < i--)
+            {
+                interval = intervals.get(i);
+                if (interval.b < from)
+                    return false;
+                if (interval.a <= to)
+                    return true;
+            }
+			return false;
+		}
+
+        Interval getChangedIntervalContaining(int from, int to)
+        {
+            Interval interval;
+            int i = intervals.size();
+            while (0 < i--)
+            {
+                interval = intervals.get(i);
+                if (interval.b < from)
+                    return null;
+                if (interval.a <= to)
+                    return interval;
+            }
+            return null;
+        }
+
+	}
+
     final ParseTree tree;
+    final Map<String, ChangedIntervalsList> changedIntervalsByProgram;
 
     public ParseTreeRewriter(ParseTree tree, TokenStream tokens) {
         super(tokens);
+		this.changedIntervalsByProgram = new HashMap();
+		this.changedIntervalsByProgram.put("default", new ChangedIntervalsList());
         this.tree = tree;
     }
+
+    // changed intervals
+
+	@Override public void rollback(String programName, int instructionIndex)
+	{
+		super.rollback(programName, instructionIndex);
+		ChangedIntervalsList changedIntervals = changedIntervalsByProgram.get(programName);
+		if (null != changedIntervals)
+			changedIntervals.rebuildFromRewriteOps(programs.get(programName));
+	}
+
+	@Override public void insertBefore(String programName, int index, Object text)
+	{
+		super.insertBefore(programName, index, text);
+		ChangedIntervalsList changedIntervals = changedIntervalsByProgram.get(programName);
+        if (null == changedIntervals)
+            changedIntervalsByProgram.put(programName, (changedIntervals = new ChangedIntervalsList()));
+		changedIntervals.changed(index, index);
+	}
+
+	@Override public void replace(String programName, int from, int to, Object text)
+	{
+		super.replace(programName, from, to, text);
+		ChangedIntervalsList changedIntervals = changedIntervalsByProgram.get(programName);
+        if (null == changedIntervals)
+            changedIntervalsByProgram.put(programName, (changedIntervals = new ChangedIntervalsList()));
+		changedIntervals.changed(from, to);
+	}
+
+	@Override public String getText(String programName, Interval interval)
+	{
+        // super.getText is very expensive - avoid it if we can.
+		ChangedIntervalsList changedIntervals = changedIntervalsByProgram.get(programName);
+		if (null != changedIntervals && !changedIntervals.isChanged(interval))
+			return this.tokens.getText(interval);
+		return super.getText(programName, interval);
+	}
 
 	// operations on an Interval
 
